d4bf9a3160ae810e8be0354aab4b7852b798d9a8
Nicholas Evans <nick@nick-evans.com>
2011-05-18 11:22:02 -0400

    Adding typekit

--- js/mylibs/desktopHelpers.js
+++ js/mylibs/desktopHelpers.js
@@ -1,6 +1,7 @@
 // header scroll
 nick.desktopBehaviours = function(){
-	$(window).scroll(function(){nick.headerScroll.init($('header'), 100)});
+	$(window).bind("scroll",function(){nick.headerScroll.init($('header'), 100)});
+	$(window).bind("resize",function(){nick.headerScroll.init($('header'), 100)});
 }
 
 
@@ -15,10 +16,13 @@ nick.headerScroll = {
 		if($header.height() > headerHeight || $(window).scrollTop() < winHeight -200){
 			var percentage = $(window).scrollTop() > 1 ? 100 - (100 * ($(window).scrollTop() / winHeight )) : 100;
 			$header.css('height', percentage + "%");
+			
+			$header.css('font-size', $header.height()/4 + "px").css('line-height', $header.height() + "px");
 			$('.head .slideshow:hidden').groupedCrossFader('startAuto').fadeIn();
 			$('.head').removeClass('head-top');
 		}else{
 			$header.css('height', headerHeight + "px");
+			$header.css('font-size', "24px").css('line-height', headerHeight + "px");
 			$('.head .slideshow').groupedCrossFader('pauseAuto').fadeOut();
 			$('.head').addClass('head-top');
 		}
